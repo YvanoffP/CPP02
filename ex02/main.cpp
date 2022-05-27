38aca7eead88fb9ff14f3d351ad6522e3bfcb8ed
YvanoffP <petruzzi.yvan@gmail.com>
2022-05-28 00:56:39 +0200

    thank you Bima42 and M0san for the help

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    Fixed const L( Fixed( 1 ) * Fixed( 2 ) );
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << Fixed::min( a, b ) << std::endl;
+
+	std::cout << std::boolalpha;
+	std::cout << b << " > " << L << ": " << (b > L) << std::endl;
+	std::cout << b << " == " << L << ": " << (b == L) << std::endl;
+
+	std::cout << (b + 10) << std::endl;
+	std::cout << (b / 2) << std::endl;
+
+
+
+    return 0;
+}
